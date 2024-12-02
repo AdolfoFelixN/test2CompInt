5f1dddcd22a40c9e7032e6c0fd25c4b892a2873c
Adolfo Felix <164815910+AdolfoFelixN@users.noreply.github.com>
2024-12-01 22:28:30 -0600

    deleteProducts()

--- client/asset/scripts/script.js
+++ client/asset/scripts/script.js
@@ -31,15 +31,17 @@ var checkout = async (event) => {
         document.getElementById("qr").style.display = "block";
         document.getElementById("image").src = image;
 
-        /*
+        
         // Opcional: ocultar el QR después de 10 segundos
         setTimeout(() => {
             document.getElementById("qr").style.display = "none";
+            deleteProducts();
         }, 10000);
-        */
+        
+        
     } catch (error) {
         console.error("Error generating QR:", error);
     }
 
-    deleteProducts();
+    //deleteProducts();
 };
